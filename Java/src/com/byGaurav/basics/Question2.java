c66d05945cfbbbdd3609f2e269f7df6ad1dd33a7
GauravAmarnani <gauravamarnani1213@gmail.com>
2021-01-17 12:03:25 +0530

    Committing Question2. (Using Intellij).

---
+++ Java/src/com/byGaurav/basics/Question2.java
@@ -0,0 +1,27 @@
+/*
+    Author @Gaurav Amarnani.
+    Published @17 January, 2021, 11:30 AM.
+    Question 1 : Sum of two numbers.
+*/
+
+package com.byGaurav.basics;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String... args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter number 1 : ");
+            Integer number1 = scanner.nextInt();
+            System.out.println("Enter number 2 : ");
+            Integer number2 = scanner.nextInt();
+            Integer total = number1 + number2;
+            System.out.println("Total : " + total);
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Improper Input.");
+            main();
+        }
+    }
+}
