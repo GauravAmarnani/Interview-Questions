f0b3b3198551d59aa4e6da152384aaa2de88c885
GauravAmarnani <gauravamarnani1213@gmail.com>
2021-01-18 19:20:09 +0530

    Modified Question 1. Now it generates Fibonacci series for number of times and also till a particular number. (Git Using Intellij).

--- Java/src/com/byGaurav/codingQuestions/Question1.java
+++ Java/src/com/byGaurav/codingQuestions/Question1.java
@@ -1,7 +1,9 @@
 /*
     Author @Gaurav Amarnani.
     Published @18 January, 2021, 6:50 PM.
-    Question : Fibonacci Series.
+    Question : Fibonacci Series
+               A. Up to a particular number.
+               B. For a particular number of times.
 */
 
 package com.byGaurav.codingQuestions;
@@ -14,9 +16,26 @@ import static java.lang.System.out;
 public class Question1 {
     public static void main(String... args) {
         Scanner scanner = new Scanner(in);
-        out.println("Enter number till which you want the fibonacci series : ");
-        Integer end = scanner.nextInt();
-        Integer first = 0, second = 1, temp = 0;
+        out.println("Enter number for fibonacci series : ");
+        Integer number = scanner.nextInt();
+        out.println("Choose A for 'Up to a particular number.' & B for 'For a particular number of times.'");
+        String choice = scanner.next();
+        switch (choice) {
+            case "A":
+                uptoNumber(number);
+                break;
+            case "B":
+                forNumberTimes(number);
+                break;
+            default:
+                out.println("Imporper Input.");
+                main();
+        }
+        scanner.close();
+    }
+
+    public static void uptoNumber(Integer end) {
+        Integer first = 1, second = 1, temp = 0;
         out.print("Fibonacci series for " + end + " :\n" + first + " " + second);
         while (second < end) {
             temp = first;
@@ -24,6 +43,17 @@ public class Question1 {
             second = temp + first;
             if (second < end)
                 out.print(" " + second);
+        }
+    }
+
+    public static void forNumberTimes(Integer times) {
+        Integer first = 1, second = 1, temp = 0;
+        out.print("Fibonacci series for " + times + " number of times :\n" + first + " " + second);
+        for (int i = 1; i < times - 1; i++) {
+            temp = first;
+            first = second;
+            second = temp + first;
+            out.print(" " + second);
         }
     }
 }
